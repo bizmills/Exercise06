d78a0cd95b497f075a682207fed47cfebf39bcd2
Hackbright Student <info@hackbrightacademy.com>
2014-06-09 16:47:34 -0700

    Complete: prints restaurant name and score

--- sorted_data.py
+++ sorted_data.py
@@ -4,6 +4,7 @@
 # input_file = open(filename)
 input_file = open("scores.txt")
 
+#builds dictionary
 restaurant_ratings = {}
 
 for line in input_file:
@@ -12,4 +13,12 @@ for line in input_file:
     rest_name = restaurant_data[0]
     rating = restaurant_data[1]
     restaurant_ratings[rest_name] = rating
-print restaurant_ratings
+
+# sorts into alphabetized tuples
+sorted_rest = (sorted(restaurant_ratings.items()))
+
+#loops through tuples and prints report.
+for i in range(len(sorted_rest)):
+    print "Restaurant '%s' is rated at %s." % (sorted_rest[i][0], sorted_rest[i][1])
+
+
